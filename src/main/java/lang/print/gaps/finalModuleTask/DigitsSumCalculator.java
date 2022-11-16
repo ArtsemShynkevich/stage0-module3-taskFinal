9511326a6f3e4c7d912166449ce89d520d2c3dcc
artem_shinkevich <SKILLBOX2021oNe>
2022-11-16 23:24:39 +0300

    All tasks done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int firstNumber = number / 1000;
+        int secondNumber = number % 1000 / 100;
+        int thirdNumber = number % 1000 % 100 / 10;
+        int fourNumber = number % 1000 % 100 % 10;
+
+        System.out.println(fourNumber + secondNumber + thirdNumber + firstNumber);
+
+
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int firstNumber = number % 10;
+        int secondNumber = (number / 10) % 10;
+        int thirdNumber = number / 100;
+
+        System.out.println(firstNumber + "" + secondNumber + "" + thirdNumber);
+
+
     }
 }
